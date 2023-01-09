27573050e14e6b69382cd9e178a8432b7e856efe
Carl Gudmundsson <komoss@gmail.com>
2023-01-09 20:43:10 +0100

    First update

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
 
-  root "events#index"
+  
 
   get '/events', to: "events#index"
+  resources :users, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "events#index"
 end
