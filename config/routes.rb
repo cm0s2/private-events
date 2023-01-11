a2e4f2ddf68a7e1ac5bb2a4bed86d16b923d908b
Carl Gudmundsson <komoss@gmail.com>
2023-01-11 13:05:22 +0100

    Add routes for events

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   
 
-  get '/events', to: "events#index"
+  # get '/events', to: "events#index"
   resources :users, only: [:show]
+  resources :events, only: [:index, :new, :create, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
