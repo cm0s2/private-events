fffcbf9268c0cd44b59865760288a24df15f9e70
Carl Gudmundsson <komoss@gmail.com>
2023-01-11 13:04:47 +0100

    Add required fields validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
+
+  validates :name, presence: true
+  validates :date, presence: true
+  validates :location, presence: true
 end
