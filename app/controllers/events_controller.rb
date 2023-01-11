511929986fb3877f094b7946839f0193f021b4ef
Carl Gudmundsson <komoss@gmail.com>
2023-01-11 13:06:25 +0100

    Add show, new and create methods for events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,27 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = current_user.events.new
+  end
+
+  def create
+    @event = current_user.build_event(event_params)
+
+    if @event.save
+      redirect_to root_path, notice: "Event was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def event_params
+      params.require(:event).permit(:name, :date, :location)
+    end
 end
