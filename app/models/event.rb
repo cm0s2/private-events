e51d87e17371340ac5819165ebd6d35f9d77c167
Carl Gudmundsson <komoss@gmail.com>
2023-01-13 13:26:10 +0100

    Change validation to one line instead of multiple lines

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
 
-  validates :name, presence: true
-  validates :date, presence: true
-  validates :location, presence: true
+  validates :name, :date, :location, presence: true
 end
