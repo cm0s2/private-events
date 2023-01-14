1f94aea4e511a8620ed370824cc5d79c2f348c72
Carl Gudmundsson <komoss@gmail.com>
2023-01-14 13:49:49 +0100

    Update schema to include event_users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_08_200901) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_13_121436) do
+  create_table "event_users", force: :cascade do |t|
+    t.integer "attendee_id", null: false
+    t.integer "attended_event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["attended_event_id"], name: "index_event_users_on_attended_event_id"
+    t.index ["attendee_id"], name: "index_event_users_on_attendee_id"
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.datetime "date"
@@ -33,5 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_08_200901) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "event_users", "events", column: "attended_event_id"
+  add_foreign_key "event_users", "users", column: "attendee_id"
   add_foreign_key "events", "users", column: "creator_id"
 end
