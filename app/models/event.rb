e51c4496f627744e4002fe2b9caa6bca342e5876
Carl Gudmundsson <komoss@gmail.com>
2023-01-14 13:48:52 +0100

    Modify event and user model to use through table

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
 
+  has_many :event_users, foreign_key: :attended_event_id
+  has_many :attendees, through: :event_users
+
   validates :name, :date, :location, presence: true
 end
