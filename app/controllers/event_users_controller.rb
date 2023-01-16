5accebb5beb288a6c4df3c51a75d04238b5e16aa
Carl Gudmundsson <komoss@gmail.com>
2023-01-16 12:38:09 +0100

    Add functionality to attend events

--- app/controllers/event_users_controller.rb
+++ app/controllers/event_users_controller.rb
@@ -1,7 +1,16 @@
 class EventUsersController < ApplicationController
-  before_action :require_login
+  before_action :authenticate_user!
 
   def create
+    event = Event.find(params[:event_id])
+    event_user = EventUser.new(attendee_id: current_user.id, attended_event_id: event.id)
+
+    if event_user.save
+      flash[:notice] = "You joined the event"
+      redirect_to events_path(event)
+    else
+      redirect_to events_path(event)
+    end
   end
 
   def destroy
