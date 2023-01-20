38d8230c9bab6d50f9bd2eea69790e8f19c3773b
Carl Gudmundsson <komoss@gmail.com>
2023-01-20 14:53:24 +0100

    Add functionality to edit events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,12 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, only: %i[ new create update destroy ]
+
   def index
     @events = Event.all
   end
 
   def show
-    @event = Event.find(params[:id])
   end
 
   def new
@@ -21,7 +23,28 @@ class EventsController < ApplicationController
     end
   end
 
+  # TODO: Make sure edit and update checks that event.craetor = current_user
+
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to event_url(@event), notice: "Event was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
     def event_params
       params.require(:event).permit(:name, :date, :location)
     end
